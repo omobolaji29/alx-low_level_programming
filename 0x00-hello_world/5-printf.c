be455f92eeceb3698889c413345a51ca114223a8
omobolaji29 <omobolagee29@gm,ail.com>
2023-03-16 18:41:19 -0700

    nhhh

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - this is the function
+ *
+ * return : 0 when successful
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
