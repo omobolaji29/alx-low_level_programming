3a498403c617ff40e495311044930bd6b2a112f7
omobolaji29 <omobolagee29@gm,ail.com>
2023-03-21 18:09:30 -0700

    1-alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_alphabet  - a function to print alphabets
+ *
+ * Return: always 0
+ */
+
+void print_alphabet(void)
+{
+	char f;
+
+	for (f = 'a'; f <= 'z'; f++)
+	_putchar(f);
+	_putchar('\n');
+}
